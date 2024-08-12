47abe04957f4bbac5ac5343447a60809deeb97be
kevin <1648053382@qq.com>
2024-08-12 16:25:39 +0800

    day-9

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -13,6 +13,7 @@ import org.apache.poi.ss.formula.functions.T;
 import com.github.pagehelper.Page;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper{
@@ -79,4 +80,10 @@ public interface DishMapper{
      */
     @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
     List<Dish> getBySetmealId(Long setmealId);
+    /**
+     * 根据条件统计菜品数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
--- sky-server/src/main/java/com/sky/service/AddressBookService.java
+++ sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -1,6 +1,12 @@
 package com.sky.service;
 
 import com.sky.entity.AddressBook;
+import com.sky.vo.BusinessDataVO;
+import com.sky.vo.DishOverViewVO;
+import com.sky.vo.OrderOverViewVO;
+import com.sky.vo.SetmealOverViewVO;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AddressBookService {
@@ -17,4 +23,33 @@ public interface AddressBookService {
 
     void deleteById(Long id);
 
+    interface WorkspaceService {
+
+        /**
+         * 根据时间段统计营业数据
+         * @param begin
+         * @param end
+         * @return
+         */
+        BusinessDataVO getBusinessData(LocalDateTime begin, LocalDateTime end);
+
+        /**
+         * 查询订单管理数据
+         * @return
+         */
+        OrderOverViewVO getOrderOverView();
+
+        /**
+         * 查询菜品总览
+         * @return
+         */
+        DishOverViewVO getDishOverView();
+
+        /**
+         * 查询套餐总览
+         * @return
+         */
+        SetmealOverViewVO getSetmealOverView();
+
+    }
 }
